810f16e06f2622b4b4cde2fd1971a833106729b4
calebkabaya <calebkabaya@gmail.com>
2022-04-09 05:14:23 +0300

    patch: created my array items

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+
+  quotes:Quote[]=[
+    new Quote(1,'Caleb M Kabaya','Whatever you do – give it 100%','Martin L. King',0,0),
+    new Quote(1,'Caleb M Kabaya','Whatever you do – give it 100%','Martin L. King',0,0),
+    new Quote(1,'Caleb M Kabaya','Whatever you do – give it 100%','Martin L. King',0,0)
+  ]
   constructor() { }
 
   ngOnInit(): void {
