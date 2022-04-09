52a8cdb8a09c66b99eb0e87072886c7faacf5b4a
calebkabaya <calebkabaya@gmail.com>
2022-04-09 06:29:45 +0300

    patch:highlight functionality

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,7 +10,7 @@ export class QuotesComponent implements OnInit {
 
   quotes:Quote[]=[
     new Quote(1,'Caleb M Kabaya','Whatever you do – give it 100%','Martin L. King',0,0),
-    new Quote(2,'Caleb M Kabaya','Facetime before you meet up','James K.',0,0),
+    new Quote(2,'Caleb M Kabaya','Facetime before you meet up','James K.',1,0),
     new Quote(3,'Caleb M Kabaya','Take a break from dating when exhausted','Elizabeth Olsen',0,0)
   ]
   constructor() { }
